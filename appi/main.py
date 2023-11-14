dd86305f26659d44d902b1ed4bd4dfe80ce428f3
Yucky <nik.fedin.2005@mail.ru>
2023-11-14 20:52:13 +0300

    БД

--- appi/main.py
+++ appi/main.py
@@ -1,5 +1,38 @@
-from fastapi import FastAPI
-app=FastAPI()
-@app.get('/')
-async def index():
-    return 'test'
+from appi.models import Base, engine, Plant, Seed, FruitTree
+
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Создание нового растения
+plant1 = Plant(name='Роза', description='Кустарник со цветами различных оттенков.', family_id=1)
+session.add(plant1)
+
+# Создание нового семени
+seed1 = Seed(name='Розовые семена', description='Семена для разведения роз.', plant_id=1)
+session.add(seed1)
+
+# Создание нового плодового дерева
+fruit_tree1 = FruitTree(name='Яблоня', description='Плодовое дерево, дающее яблоки.', plant_id=1)
+session.add(fruit_tree1)
+
+session.commit()
+
+# Получение всех растений
+plants = session.query(Plant).all()
+for plant in plants:
+    print(plant.name)
+
+# Получение информации о семенах растений
+seeds = session.query(Seed).all()
+for seed in seeds:
+    print(seed.name)
+
+# Получение информации о плодовых деревьях
+fruit_trees = session.query(FruitTree).all()
+for fruit_tree in fruit_trees:
+    print(fruit_tree.name)
+
---
+++ appi/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+engine = create_engine('sqlite:///plants.db')
+Base = declarative_base()
+
+class Plant(Base):
+    __tablename__ = 'plants'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    family_id = Column(Integer, ForeignKey('families.id'))
+
+    family = relationship("Family", back_populates="plants")
+
+
+class Family(Base):
+    __tablename__ = 'families'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+
+    plants = relationship("Plant", back_populates="family")
+
+class Seed(Base):
+    __tablename__ = 'seeds'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    plant_id = Column(Integer, ForeignKey('plants.id'))
+
+    plant = relationship("Plant", back_populates="seeds")
+
+class FruitTree(Base):
+    __tablename__ = 'fruit_trees'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    plant_id = Column(Integer, ForeignKey('plants.id'))
+
+    plant = relationship("Plant", back_populates="fruit_tree")
+
